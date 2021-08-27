125431d42a8c71b8e247134fe42e707fa782427e
KanishkCoder <76817700+KanishkCoder@users.noreply.github.com>
2021-08-27 18:53:57 +0530

    Add files via upload

---
+++ dropbox.py
@@ -0,0 +1,24 @@
+from os import access
+import dropbox
+
+class TransferData:
+    def __init__(self,access_token):
+        self.access_token=access_token
+
+    def upload_file(self,file_from,file_to):
+        dbx=dropbox.dropbox(self.access_token)
+
+        f=open(file_from,'rb')
+        dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token="sl.A3UQYVo_CYwoMA529qyDsKTE9FNCpBVxDOBcBvnIn5dVkFMUHwYyvx6KScWjLdmDCYdCQBU3gC-jkcebtcfItLjB_640Nfj3dyMxxXBv8TV2SWuHbS_MOd7wcvsNe-RcmxeuOjU"
+    transferData=TransferData(access_token)
+
+    file_from= input("Enter the file path to transfer: ")
+    file_to= input("Enter the full path to upload to dropbox: ")
+
+    transferData.upload_file(file_from,file_to)
+    print("File has been moved ! ! !")
+
+main()
